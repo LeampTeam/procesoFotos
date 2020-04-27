b55e76b9aec8a1a18e05cae192b8b8f1a65433a7
RafaelZiraldo <rafazira83@gmail.com>
2020-04-27 15:40:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var fs = require("fs")
+const Producto=require('./model/product')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/almacenBackEnd', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
+
+
+function cambiarNombre(file){
+    let ext=file.split(".")[1];
+    let date=Date.now().toString()
+   Producto.findOneAndUpdate({img:file},{img:'product_'+date+'.'+ext}, { new: true })
+   .exec(function(err,prod){
+   
+        if(prod!=null){
+        let origen='/home/rafael/programacionrafa/ProyectoAlmacenDIego/LeampTeam-BackEnd/leampteam/imagenes/producto/'+file
+        let desti='/home/rafael/programacionrafa/ProyectoAlmacenDIego/API-LeampTeam/imagenes/producto/'+prod.img
+        fs.copyFileSync(origen,desti)  
+        }
+   })
+   
+  
+   
+//    function(err,prod){
+//         let origen='/home/rafael/programacionrafa/ProyectoAlmacenDIego/LeampTeam-BackEnd/leampteam/imagenes/producto/'+file
+//         let desti='/home/rafael/programacionrafa/ProyectoAlmacenDIego/API-LeampTeam/imagenes/producto/'+prod.img
+//         fs.copyFileSync(origen,desti)  
+//         console.log(prod)
+        
+//     })
+}
+
+ fs.readdir("/home/rafael/programacionrafa/ProyectoAlmacenDIego/LeampTeam-BackEnd/leampteam/imagenes/producto/",function(err, files) {
+     console.log(files)
+    if (err) {
+       return console.error(err);
+    }
+    files.forEach( function (file) {
+        cambiarNombre(file)
+       console.log( file );
+    });
+ });
+
+ 
+
---
+++ model/product.js
@@ -0,0 +1,24 @@
+var mongoose= require('mongoose');
+var Schema =mongoose.Schema;
+
+
+var ProducSchema=Schema({
+   
+    name:String,
+    description:String,
+    price:Number,
+    code:String,  
+    stock:Number,
+    crateAt:String,
+    marca:{ type: Schema.Types.ObjectId, ref: 'Marca' },
+    img:String,
+    esFragancia:Boolean,
+    categoria: { type: Schema.Types.ObjectId, ref: 'Categoria' },
+    fragancia: { type: Schema.Types.ObjectId, ref: 'Fragancia' },
+    eliminado:Boolean
+
+})
+
+
+
+module.exports=mongoose.model('Producto',ProducSchema)
