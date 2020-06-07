d524e5e5ee50a10dda3d53b49c62cfde46c86037
ziraldodiego <ziraldodiego@gmail.com>
2020-06-07 12:28:59 -0300

    a

---
+++ borrar.js
@@ -0,0 +1,7 @@
+var os = require("os");
+
+let type = os.type();
+let plataforma = os.platform();
+
+console.log(type)
+console.log(plataforma)
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require("fs")
 var rutas = require('./local')
-
+var path = require('path');
 
 const mongoose = require('mongoose');
 const connBackEnd = mongoose.createConnection('mongodb://localhost:27017/almacenBackEnd',
@@ -15,20 +15,23 @@ let ProductoFront=connFrontEnd.model('Producto',require('./ShemaFront/producto')
 
 
    function cambiarNombre(file){
+       console.log(file)
       let i=0
       ProductoBack.findOne({img:file},function(err,produ){
+          console.log(err)
+         console.log(produ)
          if(produ!=null){
-            
+            console.log(produ)
             let  ext=file.split(".")[1];
             let  nameimg='product_'+produ._id+'.'+ext
              ProductoFront.findOneAndUpdate({code:produ.code},{img:nameimg}, { new: true },function(err,pro){
                i++
                if(pro!=null){
    
-                 let origen=rutas.rutaOrigen+file   
-                        
-                  let desti=rutas.rutaDestino+pro.img
-                  
+                 let origen = rutas.rutaOrigen + file
+                     console.log(origen)   
+                  let desti = rutas.rutaDestino + pro.img
+                     console.log(desti)
                   
                   fs.copyFile(origen,desti,function(err){
                      if(err){
@@ -43,6 +46,8 @@ let ProductoFront=connFrontEnd.model('Producto',require('./ShemaFront/producto')
             }) 
                
               
+         }else{
+               console.log('no se copio')
          }
          
       })
@@ -53,21 +58,28 @@ let ProductoFront=connFrontEnd.model('Producto',require('./ShemaFront/producto')
 
    // let origen='/home/diego/Escritorio/LimpTeam/LeampTeam-BackEnd/leampteam/imagenes/producto/'+file
    // let desti='/home/diego/Escritorio/LimpTeam/API-LeampTeam/imagenes/'+prod.img
-   let filesElim=fs.readdirSync(rutas.rutaDestino)
+   // let filesElim = fs.readdirSync(rutas.rutaDestino)
     
-      filesElim.forEach( function (file) {
-          fs.unlinkSync(rutas.rutaDestino+file)
-      })
+   //    filesElim.forEach( function (file) {
+   //        fs.unlinkSync(rutas.rutaDestino+file)
+   //    })
  
-   
-  let files=fs.readdirSync(rutas.rutaOrigen)
+   try{
+      console.log('ruta',rutas.rutaOrigen)
+   let files = fs.readdirSync(rutas.rutaOrigen)
+   console.log(files)
+   files.forEach(function (file) {
+      
+      cambiarNombre(file)
+      console.log(file)
+
+   })
+   }catch (err){
+      console.log(err)
+   }
+  
     
      
-      files.forEach( function (file) {
-        cambiarNombre(file)
-        
-      })
+      
    
-
- 
 
